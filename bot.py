988764761e5109fe5a800e020f3a63365262417e
yodgorovazamat24-stack <yodgorovazamat24@gmail.com>
2026-09-25 16:00:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -271,7 +271,7 @@ async def handle_all_messages(message: types.Message):
         
         output_template = "video.mp4"
     ydl_opts = {
-            'format': 'best',
+            'format': 'best[ext=mp4]/best',
             'outtmpl': output_template,
             'http_headers': {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
         }
